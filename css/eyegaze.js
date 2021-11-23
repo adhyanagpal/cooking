f3b7e3dbd8e718fff79cdaa652a7e190a4671e3c
sourabhsinghyadav999 <sourabh@newzera.com>
2021-11-23 17:08:35 +0530

    Eye Tracking added

---
+++ css/eyegaze.js
@@ -0,0 +1,9 @@
+window.saveDataAcrossSessions = true;
+webgazer.setGazeListener((data, elapsedTime) => {
+    if (data == null) {
+        return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(data, elapsedTime); //elapsed time is based on time since begin was called
+}).begin();
