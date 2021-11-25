bc422101b7001449361b828d0f2d7b2a5a1519e9
sourabhsinghyadav999 <sourabh@newzera.com>
2021-11-25 18:06:51 +0530

    Eye Tracking Used for Reading Recipe Added

--- eyetracking.js
+++ eyetracking.js
@@ -1,9 +1,33 @@
 window.saveDataAcrossSessions = true;
-webgazer.setGazeListener((data, elapsedTime) => {
+webgazer.setGazeListener((data, currentTime) => {
     if (data == null) {
         return;
     }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(data, elapsedTime); //elapsed time is based on time since begin was called
+    var x = data.x; //these x coordinates are relative to the viewport
+    var y = data.y; //these y coordinates are relative to the viewport
+    elem = document.elementFromPoint(x, y);
+    if(elem != null){
+        id = elem.id;
+        ids = id.split("_")
+        if(ids.length > 1){
+        let num = ids[1]
+        let collapseid = "collapse_"+num;
+        ele_value = document.getElementById(collapseid)
+        if(ele_value!=null){
+        if(ele_value.className == 'panel-collapse collapse'){
+            document.getElementById(collapseid).className = "panel-collapse collapse show"
+        }
+    }
+    }
+    }
 }).begin();
+
+
+
+
+
+
+
+
+
+
